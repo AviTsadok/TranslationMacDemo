28ce96ff84ccf0949a06e56024eeb657fdf3d5f0
Avi Tsadok <avi.tsadok@gmail.com>
2025-06-28 22:36:41 +0300

    Initial Commit

---
+++ TranslationMacDemo/TranslationMacDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslationMacDemoApp.swift
+//  TranslationMacDemo
+//
+//  Created by Avi Tsadok on 28/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct TranslationMacDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
